ef1bfbcd6a573d36317e0da7e7d4174b9b6fbfad
Vaibhav-Patel19 <vaibhav6644@gmail.com>
2022-02-06 18:08:10 +0530

    static files

--- static/js/home.js
+++ static/js/home.js
@@ -1,5 +1,5 @@
 
-function changePannel(liElement, pannel) {
+changePannel = (liElement, pannel) => {
     var liEl = document.getElementsByClassName("liEl");
     for (let i = 0; i < liEl.length; i++) {
         liEl[i].classList.remove("active");
@@ -16,3 +16,29 @@ function changePannel(liElement, pannel) {
     
     pannel.style.display = "block";
     }
+
+exploreSwitch = (clickedTab, type) => {
+    var exploreTabs = document.getElementsByClassName("exploreTab");
+
+    for(let i = 0; i < exploreTabs.length; i++){
+        exploreTabs[i].classList.remove("active");
+    }
+
+    clickedTab.classList.add("active");
+
+    var c = document.getElementsByClassName("cusines");
+
+    for(let i = 0; i < c.length; i++){
+        c[i].classList.remove("active");
+    }
+    var t = document.getElementById(type);
+    t.classList.add("active");
+
+}
+
+
+
+
+
+
+
