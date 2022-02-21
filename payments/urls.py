fd786aad1af296802c83496db5c1268b34206d52
Vaibhav-Patel19 <vaibhav6644@gmail.com>
2022-02-21 17:09:50 +0530

    Payments UI created

---
+++ payments/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("payments/", views.PaymentGateway, name="paymentsGateway"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def PaymentGateway(request):
+    return render(request, "payment.html")
